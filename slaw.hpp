8bb8d71b5d1f55aa9335b4fa570c9d57a7e946dd
iannisdezwart <iannis__@hotmail.com>
2021-11-29 17:24:56 +0100

    🧹 Fixed Types Header & Created Main Header

---
+++ slaw.hpp
@@ -0,0 +1,10 @@
+#ifndef SLAW_MAIN_H
+#define SLAW_MAIN_H
+
+#include "export.hpp"
+#include "mem.hpp"
+#include "types.hpp"
+#include "util.hpp"
+#include "vector.hpp"
+
+#endif
--- types.hpp
+++ types.hpp
@@ -1,12 +1,12 @@
-#ifndef WASM_STL_TYPES_H
-#define WASM_STL_TYPES_H
+#ifndef SLAW_TYPES_H
+#define SLAW_TYPES_H
 
 typedef unsigned char u8;
 typedef signed char i8;
 typedef unsigned short u16;
 typedef signed short i16;
-typedef unsigned int u32;
-typedef signed int i32;
+typedef unsigned long u32;
+typedef signed long i32;
 typedef unsigned long long u64;
 typedef signed long long i64;
 
