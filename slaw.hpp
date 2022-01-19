939a3630d02f3737f28160e9792f5fe8385f5de0
iannisdezwart <iannis__@hotmail.com>
2022-01-19 22:33:27 +0100

    🚀 Added New Header Files To `slaw.hpp`

--- slaw.hpp
+++ slaw.hpp
@@ -1,10 +1,11 @@
 #ifndef SLAW_MAIN_H
 #define SLAW_MAIN_H
 
-#include "export.hpp"
-#include "mem.hpp"
-#include "types.hpp"
 #include "util.hpp"
+#include "types.hpp"
+#include "io.hpp"
+#include "mem.hpp"
+#include "export.hpp"
 #include "math.hpp"
 #include "vector.hpp"
 #include "string.hpp"
